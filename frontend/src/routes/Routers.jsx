84621039ef457aad0329626b3fc6057c6c3b39ab
Avashbhandari01 <avash115@gmail.com>
2024-06-21 15:55:02 +0545

    Routing setup and pages

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Doctors from "../pages/doctors/Doctors";
+import DoctorDetails from "../pages/doctors/DoctorDetails";
+
+import { Routes, Route } from "react-router-dom";
 
 function Routers() {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+  );
 }
 
 export default Routers;
